8895a6207b0eaa1831df8976570e076a487eb9e9
Akhmedkhanov Gasan <ldagonl@gmail.com>
2025-05-30 13:13:58 +0300

    30-05

--- app1/main.py
+++ app1/main.py
@@ -1,26 +1,57 @@
-todos = []
-
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.append(todo)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show':
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}"
+                row = f"{index + 1}-{item.strip("\n")}"
                 print(row)
+
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + "\n"
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip("\n")
+            todos.pop(index)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
         case 'exit':
             break
         case _:
             print("Command is not valid.")  
-    print(todos)
 
-print("Bye!")
+print("Fuck off!")
