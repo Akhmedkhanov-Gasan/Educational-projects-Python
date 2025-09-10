d89f1e9c3e5e374843c8e58aaed83239c24b8bc1
Akhmedkhanov-Gasan <ldagonl@gmail.com>
2025-09-10 14:26:12 +0300

    09-10-2025

--- NumPy/11.Array Math.py
+++ NumPy/11.Array Math.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-array_a = np.array([[1,2],[3,4]])
-array_b = np.array([[2, 2],[6,6]])
+array_a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+array_b = np.array([[2,2,2],[3,4,5],[6,7,8]])
 
 # single array operations
 # print(array_a.sum(axis=1))
@@ -15,4 +15,16 @@ array_b = np.array([[2, 2],[6,6]])
 # print(array_a * array_b)
 # print(array_a / array_b)
 
-print(np.dot(array_a, array_b))
+# print(np.dot(array_a, array_b))
+
+print(array_a)
+print(array_b)
+
+print(array_a.sum(axis=0))
+
+print(np.ptp(array_a, axis=1))
+
+print(array_a.mean())
+
+#power
+print(np.power(array_a, array_b))
--- contests/4/4.1/4.1.18.py
+++ contests/4/4.1/4.1.18.py
@@ -1,7 +1,19 @@
 def merge(left, right):
     new_list = []
-    
+    i = j = 0
 
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            new_list.append(left[i])
+            i += 1
+        else:
+            new_list.append(right[j])
+            j += 1
+
+    new_list.extend(left[i:])
+    new_list.extend(right[j:])
+
+    return tuple(new_list)
 
 result = merge((1, 2), (3, 4, 5))
 print(result)
