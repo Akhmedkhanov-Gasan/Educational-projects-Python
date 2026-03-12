9b76bf04e5f79c66d6b039d1aa9b1c0e7f323c4c
Akhmedkhanov-Gasan <ldagonl@gmail.com>
2026-03-12 11:27:18 +0300

    03-12-2026 2

--- demo/03.12.2026.py
+++ demo/03.12.2026.py
@@ -30,3 +30,20 @@ def find_vowel(letters, k):
             best = max(best, current_best)
     return best
 
+def valid_parentheses(s):
+    stack = []
+    pairs = {')': '(', ']': '[', '}': '{'}
+    for char in s:
+
+        if char not in pairs:
+            stack.append(char)
+        else:
+            if stack == []:
+                return False
+            if stack[-1] != pairs[char]:
+                return False
+            stack.pop()
+
+    return not stack
+
+print(valid_parentheses('([{}])'))
