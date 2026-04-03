98f743783838bc536b7bb9511cb3e07457236826
Akhmedkhanov-Gasan <ldagonl@gmail.com>
2026-04-03 15:15:20 +0300

    add to two_poibters

--- demo/two_pointers.py
+++ demo/two_pointers.py
@@ -21,3 +21,22 @@ def is_palindrome(s):
 
     return True
 
+
+def reverse(nums, left, right):
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
+    return nums
+
+
+def rotate(nums: list[int], k: int) -> None:
+    k = k % len(nums)
+    n = len(nums)
+    reverse(nums, 0, n - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, n - 1)
+
+nums = ([1,2,3,4,5,6,7])
+rotate(nums, 3)
+print(nums)
