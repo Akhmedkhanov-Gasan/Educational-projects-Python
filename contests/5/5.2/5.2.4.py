bb1c2723f11bcb35278e658639b039dddc20e77d
Akhmedkhanov-Gasan <ldagonl@gmail.com>
2026-04-16 13:46:03 +0300

    add 5.2.4

---
+++ contests/5/5.2/5.2.4.py
@@ -0,0 +1,52 @@
+from math import gcd
+
+
+class Fraction:
+
+    def __init__(self, *args):
+        if len(args) == 2:
+            num = args[0]
+            den = args[1]
+        elif len(args) == 1:
+            num_str, den_str = args[0].split('/')
+            num = int(num_str)
+            den = int(den_str)
+        else:
+            raise ValueError("Invalid arguments")
+
+        self._numerator = num
+        self._denominator = den
+        self._reduce()
+
+    def __str__(self):
+        return f"{self._numerator}/{self._denominator}"
+
+    def _reduce(self):
+        g = gcd(self._numerator, self._denominator)
+        self._numerator //= g
+        self._denominator //= g
+
+    def numerator(self, value=None):
+        if value is None:
+            return abs(self._numerator)
+        else:
+            self._numerator = value
+            self._reduce()
+
+    def denominator(self, value=None):
+        if value is None:
+            return abs(self._denominator)
+        else:
+            self._denominator = value
+            self._reduce()
+
+    def __repr__(self):
+        return f"Fraction({self._numerator}, {self._denominator})"
+
+
+fraction = Fraction(3, 210)
+print(fraction, repr(fraction))
+fraction.numerator(10)
+print(fraction.numerator(), fraction.denominator())
+fraction.denominator(2)
+print(fraction.numerator(), fraction.denominator())
