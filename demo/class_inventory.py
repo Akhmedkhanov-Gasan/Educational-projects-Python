04306624d82ba4de5bab5d43622ec8dc51508855
Akhmedkhanov-Gasan <ldagonl@gmail.com>
2026-04-20 12:36:14 +0300

    update Inventory v0.2

--- demo/class_inventory.py
+++ demo/class_inventory.py
@@ -1,57 +1,72 @@
 class Inventory:
     def __init__(self):
-        self._items = []
+        self._items = {}
 
     def __str__(self):
         return f'Inventory: {self._items}'
 
     def __len__(self):
-        return len(self._items)
+        count = 0
+        for value in self._items.values():
+            count += value
+        return count
 
-
-    def __contains__(self, item):
-        return item in self._items
+    # def __contains__(self, item):
+    #     return item in self._items
 
     def __add__(self, new_item):
         new_inv = Inventory()
         new_list = self._items.copy()
-        new_list.append(new_item)
+        if isinstance(new_item, str):
+            if new_item in new_list:
+                new_list[new_item] += 1
+            else:
+                new_list[new_item] = 1
+        elif isinstance(new_item, Inventory):
+            for key, value in new_item._items.items():
+                if key in new_list:
+                    new_list[key] += value
+                else:
+                    new_list[key] = value
         new_inv._items = new_list
         return new_inv
 
-    def __iadd__(self, other):
-        self._items.append(other)
-        return self
-
-    def __eq__(self, other):
-        if not isinstance(other, Inventory):
-            return False
-        return self._items == other._items
+    # def __iadd__(self, other):
+    #     self._items.append(other)
+    #     return self
+    #
+    # def __eq__(self, other):
+    #     if not isinstance(other, Inventory):
+    #         return False
+    #     return self._items == other._items
 
     def __iter__(self):
-        return iter(self._items)
+        # return iter(self._items.items())
+        for key, value in self._items.items():
+            for _ in range(value):
+                yield key
 
     def add(self, item):
-        self._items.append(item)
+        if isinstance(item, str):
+            if item in self._items:
+                self._items[item] += 1
+            else:
+                self._items[item] = 1
+
+        elif isinstance(item, Inventory):
+            for key, value in item._items.items():
+                if key in self._items:
+                    self._items[key] += value
+                else:
+                    self._items[key] = value
 
 
 inv = Inventory()
 inv.add("sword")
 inv.add("shield")
 
-print(inv)              # Inventory: ['sword', 'shield']
-print(len(inv))         # 2
-print("sword" in inv)   # True
 
 inv2 = inv + "potion"
 
-print(inv)              # старый не изменился
-print(inv2)             # новый с potion
+inv.add(inv2)
 
-inv += "ring"
-print(inv)
-
-for item in inv:
-    print(item)
-
-print(inv == inv2)      # False
