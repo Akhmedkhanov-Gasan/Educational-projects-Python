811317ec831d490772bd043e449c7cf7a69ab7de
Akhmedkhanov-Gasan <ldagonl@gmail.com>
2026-04-20 16:53:07 +0300

    update Inventory v0.4

--- demo/class_inventory.py
+++ demo/class_inventory.py
@@ -1,4 +1,5 @@
 class Inventory:
+    # version 0.4
     def __init__(self):
         self._items = {}
 
@@ -11,8 +12,8 @@ class Inventory:
             count += value
         return count
 
-    # def __contains__(self, item):
-    #     return item in self._items
+    def __contains__(self, item):
+        return item in self._items
 
     def __add__(self, new_item):
         new_inv = Inventory()
@@ -31,17 +32,50 @@ class Inventory:
         new_inv._items = new_list
         return new_inv
 
-    # def __iadd__(self, other):
-    #     self._items.append(other)
-    #     return self
-    #
-    # def __eq__(self, other):
-    #     if not isinstance(other, Inventory):
-    #         return False
-    #     return self._items == other._items
+    def __sub__(self, item):
+        new_inv = Inventory()
+        new_list = self._items.copy()
+        if isinstance(item, str):
+            if item not in new_list:
+                new_inv._items = new_list
+                return new_inv
+            if item in new_list and new_list[item] >= 2:
+                new_list[item] -= 1
+            else:
+                new_list.pop(item)
+        elif isinstance(item, Inventory):
+            for key, value in item._items.items():
+                if key not in new_list:
+                    continue
+                new_element = new_list[key] - value
+                if new_element > 0:
+                    new_list[key] = new_element
+                elif new_element <= 0:
+                    new_list.pop(key)
+        new_inv._items = new_list
+        return new_inv
+
+    def __iadd__(self, other):
+        if isinstance(other, str):
+            if other in self._items:
+                self._items[other] += 1
+            else:
+                self._items[other] = 1
+
+        elif isinstance(other, Inventory):
+            for key, value in other._items.items():
+                if key in self._items:
+                    self._items[key] += value
+                else:
+                    self._items[key] = value
+        return self
+
+    def __eq__(self, other):
+        if not isinstance(other, Inventory):
+            return False
+        return self._items == other._items
 
     def __iter__(self):
-        # return iter(self._items.items())
         for key, value in self._items.items():
             for _ in range(value):
                 yield key
@@ -62,11 +96,10 @@ class Inventory:
 
 
 inv = Inventory()
-inv.add("sword")
-inv.add("shield")
+inv += "sword"
 
+inv2 = Inventory()
+inv2 += "sword"
+inv2 += "sword"
 
-inv2 = inv + "potion"
-
-inv.add(inv2)
-
+print(list(inv - inv2))
